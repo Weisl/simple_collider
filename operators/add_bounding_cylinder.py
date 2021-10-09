8e1e3e086936b10a51cb669918e9ed03d8de9666
matthias <Weisl@users.noreply.github.com>
2021-10-09 17:42:24 +0300

    #53  Cylindrical Collision: Support Global Space for Edit Mode

--- operators/add_bounding_cylinder.py
+++ operators/add_bounding_cylinder.py
@@ -133,18 +133,76 @@ class OBJECT_OT_add_bounding_cylinder(OBJECT_OT_add_bounding_object, Operator):
         # CLEANUP
         super().execute(context)
 
-        for i, obj in enumerate(context.selected_objects.copy()):
-            # skip if invalid object
-            if obj is None:
-                continue
+        target_object_mode = []
+        target_edit_mode = []
 
-            # skip non Mesh objects like lamps, curves etc.
-            if obj.type != "MESH":
-                continue
+        if self.obj_mode == 'EDIT':
+            for i, obj in enumerate(context.selected_objects.copy()):
 
-            initial_mod_state = {}
+                # skip if invalid object
+                if obj is None:
+                    continue
 
-            if obj.mode == "OBJECT":
+                # skip non Mesh objects like lamps, curves etc.
+                if obj.type != "MESH":
+                    continue
+
+                bounding_cylinder_data = {}
+                me = obj.data
+
+                if scene.my_use_modifier_stack == False:
+                    # Get a BMesh representation
+                    me.update()
+                    bm = bmesh.from_edit_mesh(me)
+                    vertices = [v for v in bm.verts if v.select == True]
+
+                else:  # scene.my_use_modifier_stack == True
+
+                    # Get mesh information with the modifiers applied
+                    me.update()
+                    depsgraph = bpy.context.evaluated_depsgraph_get()
+                    bm = bmesh.new()
+                    bm.from_object(obj, depsgraph)
+                    bm.verts.ensure_lookup_table()
+                    vertices = [v for v in bm.verts if v.select == True]
+
+
+                positionsX, positionsY, positionsZ = self.get_point_positions(obj, scene.my_space, vertices)
+
+                dimensions = self.generate_dimensions_WS(positionsX, positionsY, positionsZ)
+                bounding_box = self.generate_bounding_box(positionsX, positionsY, positionsZ)
+
+                if self.cylinder_axis == 'X':
+                    radius = calc_hypothenuse(dimensions[1], dimensions[2])
+                    depth = dimensions[0]
+
+                elif self.cylinder_axis == 'Y':
+                    radius = calc_hypothenuse(dimensions[0], dimensions[2])
+                    depth = dimensions[1]
+
+                else:
+                    radius = calc_hypothenuse(dimensions[0], dimensions[1])
+                    depth = dimensions[2]
+
+                bounding_cylinder_data['parent'] = obj
+                bounding_cylinder_data['radius'] = radius
+                bounding_cylinder_data['depth'] = depth
+                bounding_cylinder_data['bbox'] = bounding_box
+                target_edit_mode.append(bounding_cylinder_data)
+
+        else: #self.obj_mode == 'OBJECT':
+
+            for i, obj in enumerate(context.selected_objects.copy()):
+                # skip if invalid object
+                if obj is None:
+                    continue
+
+                # skip non Mesh objects like lamps, curves etc.
+                if obj.type != "MESH":
+                    continue
+
+                initial_mod_state = {}
+
                 if scene.my_use_modifier_stack == False:
                     for mod in obj.modifiers:
                         initial_mod_state[mod.name] = mod.show_viewport
@@ -200,21 +258,66 @@ class OBJECT_OT_add_bounding_cylinder(OBJECT_OT_add_bounding_object, Operator):
                     elif self.cylinder_axis == 'Y':
                         new_collider.rotation_euler.rotate_axis("X", radians(90))
 
-
                 # Reset modifiers of target mesh to initial state
                 if scene.my_use_modifier_stack == False:
                     for mod_name, value in initial_mod_state.items():
-                        print("key %s and value %s" % (mod_name, value))
                         obj.modifiers[mod_name].show_viewport = value
 
+                target_object_mode.append(new_collider)
 
-            prefs = context.preferences.addons["CollisionHelpers"].preferences
-            type_suffix = prefs.convexColSuffix
-            new_collider.name = super().collider_name(context, type_suffix, i + 1)
+        bpy.ops.object.mode_set(mode='OBJECT')
 
-            self.new_colliders_list.append(new_collider)
-            self.custom_set_parent(context, obj, new_collider)
-            self.primitive_postprocessing(context, new_collider, self.physics_material_name)
+        if self.obj_mode == 'EDIT':
+            for bounding_cylinder_data in target_edit_mode:
+                global tmp_name
+
+                print(str(bounding_cylinder_data))
+
+                parent = bounding_cylinder_data['parent']
+                radius = bounding_cylinder_data['radius']
+                depth = bounding_cylinder_data['depth']
+                bbox = bounding_cylinder_data['bbox']
+
+                # add new cylindrical mesh
+                bpy.ops.mesh.primitive_cylinder_add(vertices=self.vertex_count,
+                                                    radius=radius,
+                                                    depth=depth)
+
+                new_collider = context.object
+                new_collider.name = tmp_name
+
+                # align newly created object to base mesh
+                centreBase = sum((Vector(b) for b in bbox), Vector()) / 8.0
+                new_collider.location = centreBase
+
+                if scene.my_space == 'LOCAL':
+                    new_collider.rotation_euler = parent.rotation_euler
+
+                if self.cylinder_axis == 'X':
+                    new_collider.rotation_euler.rotate_axis("Y", radians(90))
+                elif self.cylinder_axis == 'Y':
+                    new_collider.rotation_euler.rotate_axis("X", radians(90))
+
+                prefs = context.preferences.addons["CollisionHelpers"].preferences
+                type_suffix = prefs.convexColSuffix
+                new_collider.name = super().collider_name(context, type_suffix, i + 1)
+
+                self.new_colliders_list.append(new_collider)
+                self.custom_set_parent(context, obj, new_collider)
+                self.primitive_postprocessing(context, new_collider, self.physics_material_name)
+
+
+        else: #   if obj.mode == 'OBJECT':
+
+            for new_collider in target_object_mode:
+
+                prefs = context.preferences.addons["CollisionHelpers"].preferences
+                type_suffix = prefs.convexColSuffix
+                new_collider.name = super().collider_name(context, type_suffix, i + 1)
+
+                self.new_colliders_list.append(new_collider)
+                self.custom_set_parent(context, obj, new_collider)
+                self.primitive_postprocessing(context, new_collider, self.physics_material_name)
 
         # Initial state has to be restored for the modal operator to work. If not, the result will break once changing the parameters
         super().reset_to_initial_state(context)
