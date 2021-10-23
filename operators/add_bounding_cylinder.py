20390b4b7463fc53152b1e5cb57a854a1e3204b0
matthias <Weisl@users.noreply.github.com>
2021-10-23 19:02:27 +0300

    Refactor cylindrical collision

--- operators/add_bounding_cylinder.py
+++ operators/add_bounding_cylinder.py
@@ -124,144 +124,63 @@ class OBJECT_OT_add_bounding_cylinder(OBJECT_OT_add_bounding_object, Operator):
         target_object_mode = []
         target_edit_mode = []
 
-        if self.obj_mode == 'EDIT':
-            for obj in context.selected_objects.copy():
 
-                # skip if invalid object
-                if obj is None:
-                    continue
+        for obj in context.selected_objects.copy():
 
-                # skip non Mesh objects like lamps, curves etc.
-                if obj.type != "MESH":
-                    continue
+            # skip if invalid object
+            if obj is None:
+                continue
 
-                bounding_cylinder_data = {}
-                me = obj.data
+            # skip non Mesh objects like lamps, curves etc.
+            if obj.type != "MESH":
+                continue
 
+            bounding_cylinder_data = {}
+            me = obj.data
+
+            if self.obj_mode == 'EDIT':
                 vertices = self.get_vertices_Edit(obj, use_modifiers=self.my_use_modifier_stack)
+            else:
+                vertices = self.get_vertices_Object(obj, use_modifiers=self.my_use_modifier_stack)
 
-                positionsX, positionsY, positionsZ = self.get_point_positions(obj, scene.my_space, vertices)
+            positionsX, positionsY, positionsZ = self.get_point_positions(obj, scene.my_space, vertices)
+            dimensions = self.generate_dimensions_WS(positionsX, positionsY, positionsZ)
+            bounding_box = self.generate_bounding_box(positionsX, positionsY, positionsZ)
 
-                dimensions = self.generate_dimensions_WS(positionsX, positionsY, positionsZ)
-                bounding_box = self.generate_bounding_box(positionsX, positionsY, positionsZ)
+            radius, depth = self.generate_radius_depth(dimensions)
 
-                radius, depth = self.generate_radius_depth(dimensions)
-
-                bounding_cylinder_data['parent'] = obj
-                bounding_cylinder_data['radius'] = radius
-                bounding_cylinder_data['depth'] = depth
-                bounding_cylinder_data['bbox'] = bounding_box
-                target_edit_mode.append(bounding_cylinder_data)
-
-        else: #self.obj_mode == 'OBJECT':
-
-            for obj in context.selected_objects.copy():
-                # skip if invalid object
-                if obj is None:
-                    continue
-
-                # skip non Mesh objects like lamps, curves etc.
-                if obj.type != "MESH":
-                    continue
-
-                initial_mod_state = {}
-                bounding_cylinder_data = {}
-
-                if self.my_use_modifier_stack == False:
-                    for mod in obj.modifiers:
-                        initial_mod_state[mod.name] = mod.show_viewport
-                        mod.show_viewport = False
-                    context.view_layer.update()
-
-                if scene.my_space == 'LOCAL':
-                    radius, depth = self.generate_radius_depth(obj.dimensions)
-
-                    # align newly created object to base mesh
-                    centreBase = sum((Vector(b) for b in obj.bound_box), Vector()) / 8.0
-                    global_bbox_center = obj.matrix_world @ centreBase
-
-                    new_collider = self.generate_cylinder_object(context, radius, depth,global_bbox_center, rotation_euler= obj.rotation_euler)
-
-                else:  # Space == 'GLOBAL'
-
-                    if self.my_use_modifier_stack == False:
-                        vertices = obj.data.vertices
-
-                    if self.my_use_modifier_stack == True:
-                        # Get mesh information with the modifiers applied
-                        depsgraph = bpy.context.evaluated_depsgraph_get()
-                        bm = bmesh.new()
-                        bm.from_object(obj, depsgraph)
-                        bm.verts.ensure_lookup_table()
-                        vertices = bm.verts
-
-                    positionsX, positionsY, positionsZ = self.get_point_positions(obj, 'GLOBAL', vertices)
-                    dimensions = self.generate_dimensions_WS(positionsX, positionsY, positionsZ)
-                    bounding_box = self.generate_bounding_box(positionsX, positionsY, positionsZ)
-
-                    radius, depth = self.generate_radius_depth(dimensions)
-
-                    # align newly created object to base mesh
-                    centreBase = sum((Vector(b) for b in bounding_box), Vector()) / 8.0
-                    global_bbox_center = centreBase
-
-                    new_collider = self.generate_cylinder_object(context, radius, depth, global_bbox_center)
-
-
-                # Reset modifiers of target mesh to initial state
-                if self.my_use_modifier_stack == False:
-                    for mod_name, value in initial_mod_state.items():
-                        obj.modifiers[mod_name].show_viewport = value
-
-
-                bounding_cylinder_data['parent'] = obj
-                bounding_cylinder_data['collider'] = new_collider
-
-                target_object_mode.append(bounding_cylinder_data)
+            bounding_cylinder_data['parent'] = obj
+            bounding_cylinder_data['radius'] = radius
+            bounding_cylinder_data['depth'] = depth
+            bounding_cylinder_data['bbox'] = bounding_box
+            target_edit_mode.append(bounding_cylinder_data)
 
         bpy.ops.object.mode_set(mode='OBJECT')
 
-        if self.obj_mode == 'EDIT':
-            for bounding_cylinder_data in target_edit_mode:
-                global tmp_name
+        for bounding_cylinder_data in target_edit_mode:
+            global tmp_name
 
-                parent = bounding_cylinder_data['parent']
-                radius = bounding_cylinder_data['radius']
-                depth = bounding_cylinder_data['depth']
-                bbox = bounding_cylinder_data['bbox']
+            parent = bounding_cylinder_data['parent']
+            radius = bounding_cylinder_data['radius']
+            depth = bounding_cylinder_data['depth']
+            bbox = bounding_cylinder_data['bbox']
 
-                # align newly created object to base mesh
-                centreBase = sum((Vector(b) for b in bbox), Vector()) / 8.0
+            # align newly created object to base mesh
+            centreBase = sum((Vector(b) for b in bbox), Vector()) / 8.0
 
-                if scene.my_space == 'LOCAL':
-                    new_collider = self.generate_cylinder_object(context, radius, depth, centreBase,
-                                                        rotation_euler=parent.rotation_euler)
-                else:
-                    new_collider = self.generate_cylinder_object(context, radius, depth, centreBase)
+            if scene.my_space == 'LOCAL':
+                new_collider = self.generate_cylinder_object(context, radius, depth, centreBase,
+                                                    rotation_euler=parent.rotation_euler)
+            else:
+                new_collider = self.generate_cylinder_object(context, radius, depth, centreBase)
 
-                self.new_colliders_list.append(new_collider)
-                self.custom_set_parent(context, parent, new_collider)
-                collections = parent.users_collection
-                self.primitive_postprocessing(context, new_collider, collections, self.physics_material_name)
+            self.new_colliders_list.append(new_collider)
+            self.custom_set_parent(context, parent, new_collider)
+            collections = parent.users_collection
+            self.primitive_postprocessing(context, new_collider, collections, self.physics_material_name)
 
-                new_collider.name = super().collider_name(basename=parent.name)
+            new_collider.name = super().collider_name(basename=parent.name)
 
-
-        else: #   if self.obj_mode == 'OBJECT':
-
-            for bounding_cylinder_data in target_object_mode:
-
-                new_collider = bounding_cylinder_data['collider']
-                parent = bounding_cylinder_data['parent']
-
-                self.new_colliders_list.append(new_collider)
-                self.custom_set_parent(context, parent, new_collider)
-                collections = parent.users_collection
-                self.primitive_postprocessing(context, new_collider, collections, self.physics_material_name)
-
-                new_collider.name = super().collider_name(basename=parent.name)
-
-        # Initial state has to be restored for the modal operator to work. If not, the result will break once changing the parameters
         super().reset_to_initial_state(context)
 
         return {'RUNNING_MODAL'}
