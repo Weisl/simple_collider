6320efafc822d8781a71d9c2955acb91c267cd8c
matthias <Weisl@users.noreply.github.com>
2021-11-01 20:49:43 +0200

    Improve UI/UX

--- ui/properties_panels.py
+++ ui/properties_panels.py
@@ -20,17 +20,22 @@ class CollissionPanel(Panel):
         obj = context.object
         scene = context.scene
 
-        row = layout.row()
-        row.operator("mesh.add_bounding_box")
+        # Physics Materials
+        layout.separator()
 
         row = layout.row()
-        row.operator("mesh.add_bounding_cylinder")
+        row.label(text='Physics Materials')
 
         row = layout.row()
-        row.prop(scene, "PhysicsIdentifier")
+        row.prop(scene, "PhysicsIdentifier", text='Material Filter')
 
         row = layout.row()
         row.prop(scene, "CollisionMaterials")
+
+
+
+        # Visibillity and Selection
+        layout.separator()
 
         col = self.layout.column_flow(columns=5, align = True)
 
@@ -57,19 +62,35 @@ class CollissionPanel(Panel):
             op.invert = True
             op.mode = key
 
+        # Conversion
+        layout.separator()
+
         row = layout.row(align=True)
         row.operator('object.convert_to_collider', icon='PHYSICS')
         row = layout.row(align=True)
         row.operator('object.convert_to_mesh', icon='MESH_MONKEY')
 
-        row = layout.row(align=True)
-        row.prop(scene, 'my_color')
-        row = layout.row(align=True)
-        row.prop(scene, 'my_color_simple')
-        row = layout.row(align=True)
-        row.prop(scene, 'my_color_complex')
+        # Create Collider
+        layout.separator()
 
+        row = layout.row(align=True)
+        row.operator("mesh.add_bounding_box", icon='MESH_CUBE')
+        row = layout.row(align=True)
+        row.operator("mesh.add_bounding_cylinder", icon='MESH_CYLINDER')
+        row = layout.row(align=True)
+        row.operator("mesh.add_bounding_convex_hull", icon='MESH_ICOSPHERE')
+        row = layout.row(align=True)
+        row.operator("mesh.add_mesh_collision", icon='MESH_MONKEY')
+        row = layout.row(align=True)
+        row.operator("mesh.add_bounding_sphere", icon='MESH_UVSPHERE')
 
-        view = context.space_data
-        shading = view.shading
+        #special Collider Creation
+        layout.separator()
+
+        prefs = context.preferences.addons["CollisionHelpers"].preferences
+        row = layout.row(align=True)
+        if prefs.executable_path:
+            row.operator("collision.vhacd", text="Convex Decomposition")
+        else:
+            row.operator("wm.url_open", text="Convex decomposition: Requires V-HACD", icon='WARNING').url = "https://github.com/kmammou/v-hacd"
 
