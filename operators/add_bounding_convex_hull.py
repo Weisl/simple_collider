8ed95931eafcf575bcf8d740818b4dccc6d148c3
Matthias Patscheider <Weisl@users.noreply.github.com>
2022-07-26 17:47:21 +0300

    Initial implementation Convex Collision Selection

--- operators/add_bounding_convex_hull.py
+++ operators/add_bounding_convex_hull.py
@@ -39,8 +39,12 @@ class OBJECT_OT_add_convex_hull(OBJECT_OT_add_bounding_object, Operator):
         # CLEANUP
         super().execute(context)
 
-        target_objects = []
+        scene = context.scene
         self.type_suffix = self.prefs.convexColSuffix
+
+        #List for storing dictionaries of data used to generate the collision meshes
+        collider_data = []
+        all_used_vertices = []
 
         # Duplicate original meshes to convert to collider
         for obj in self.selected_objects:
@@ -52,63 +56,88 @@ class OBJECT_OT_add_convex_hull(OBJECT_OT_add_bounding_object, Operator):
             if obj.type != "MESH":
                 continue
 
-            collider_data = {}
-
-            # update mesh when changing selection in edit mode etc.
-            obj.update_from_editmode()
-
-            # duplicate object
-            new_collider = obj.copy()
-            new_collider.data = obj.data.copy()
-
-            context.scene.collection.objects.link(new_collider)
-
-            if self.obj_mode == "OBJECT":
-                self.custom_set_parent(context, obj, new_collider)
-
-            else: #self.obj_mode == 'EDIT'
-                bpy.ops.object.mode_set(mode='OBJECT')
-                self.custom_set_parent(context, obj, new_collider)
-
-            if self.my_use_modifier_stack:
-                self.apply_all_modifiers(context, new_collider)
-
-            obj.select_set(False)
-
-            collider_data['parent'] = obj
-            collider_data['convex_collider'] = new_collider
-
-            target_objects.append(collider_data)
-
-        for collider_data in target_objects:
-
-            parent = collider_data['parent']
-            new_collider = collider_data['convex_collider']
-
-            new_collider.select_set(True)
-            context.view_layer.objects.active = new_collider
+            convex_collision_data = {}
 
             if self.obj_mode == "EDIT":
-                bpy.ops.object.mode_set(mode='EDIT')
-                bpy.ops.mesh.select_all(action='INVERT')
-                bpy.ops.mesh.delete(type='FACE')
+                used_vertices = self.get_vertices_Edit(obj, use_modifiers=self.my_use_modifier_stack)
 
-            else:
-                bpy.ops.object.mode_set(mode='EDIT')
+            else:  # self.obj_mode  == "OBJECT":
+                used_vertices = self.get_vertices_Object(obj, use_modifiers=self.my_use_modifier_stack)
 
-            bpy.ops.mesh.select_all(action='SELECT')
-            bpy.ops.mesh.convex_hull()
-            bpy.ops.object.mode_set(mode='OBJECT')
+            if used_vertices == None: # Skip object if there is no Mesh data to create the collider
+                continue
 
-            self.remove_all_modifiers(context, new_collider)
+            if scene.creation_mode == 'INDIVIDUAL':
+                # update mesh when changing selection in edit mode etc.
+                obj.update_from_editmode()
+
+                # duplicate object
+                convex_collision_data['parent'] = obj
+                convex_collision_data['verts_loc'] = used_vertices
+                collider_data.append(convex_collision_data)
+
+            else: #if scene.creation_mode == 'SELECTION':
+                all_used_vertices = all_used_vertices + list(used_vertices)
+
+
+        if scene.creation_mode == 'SELECTION':
+            convex_collision_data = {}
+            convex_collision_data['parent'] = self.active_obj
+            convex_collision_data['verts_loc'] = all_used_vertices
+            collider_data.append(convex_collision_data)
+
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+        for convex_collision_data in collider_data:
+            # get data from dictionary
+            parent = convex_collision_data['parent']
+            verts_loc = convex_collision_data['verts_loc']
+
+            bm = bmesh.new()
+
+            for v in verts_loc:
+                bm.verts.new(v.co)  # add a new vert
+
+            ch = bmesh.ops.convex_hull(bm, input=bm.verts)
+
+            bmesh.ops.delete(
+                bm,
+                geom=ch["geom_unused"],
+                context='VERTS',
+            )
+
+            me = bpy.data.meshes.new("mesh")
+            bm.to_mesh(me)
+            bm.free()
+
+            new_collider = bpy.data.objects.new('asd', me)
+            context.scene.collection.objects.link(new_collider)
+
+            matrix = parent.matrix_world
+            new_collider.parent = parent
+            new_collider.matrix_world = matrix
 
             # save collision objects to delete when canceling the operation
+            self.new_colliders_list.append(new_collider)
             collections = parent.users_collection
             self.primitive_postprocessing(context, new_collider, collections)
 
-            new_collider.name = super().collider_name(basename=new_collider.parent.name)
+            parent_name = parent.name
+            new_collider.name = super().collider_name(basename=parent_name)
 
-        self.new_colliders_list = set(context.scene.objects) - self.old_objs
+        # for collider_data in target_objects:
+        #
+        #     parent = collider_data['parent']
+        #     new_collider = collider_data['convex_collider']
+        #
+        #     # save collision objects to delete when canceling the operation
+        #     self.new_colliders_list.append(new_collider)
+        #     collections = parent.users_collection
+        #     self.primitive_postprocessing(context, new_collider, collections)
+        #
+        #     parent_name = parent.name
+        #     new_collider.name = super().collider_name(basename=parent_name)
+
 
         # Initial state has to be restored for the modal operator to work. If not, the result will break once changing the parameters
         super().reset_to_initial_state(context)
