7e91739f7d579e8cf93402eb9d51ba4502bff2f0
Matthias Patscheider <Weisl@users.noreply.github.com>
2022-08-25 10:22:09 +0300

    Switch display type to SOLID for group and material view

--- collider_operators/visibility_settings.py
+++ collider_operators/visibility_settings.py
@@ -8,6 +8,7 @@ class VIEW3D_OT_object_view(bpy.types.Operator):
     bl_description = 'Change viewport color type to: Collider Groups'
 
     def execute(self, context):
+        context.scene.display_type = 'SOLID'
         context.space_data.shading.type = 'SOLID'
         context.space_data.shading.color_type = 'OBJECT'
         return {'FINISHED'}
@@ -20,6 +21,7 @@ class VIEW3D_OT_material_view(bpy.types.Operator):
     bl_description = 'Change viewport color type to: Physics Material'
 
     def execute(self, context):
+        context.scene.display_type = 'SOLID'
         context.space_data.shading.type = 'SOLID'
         context.space_data.shading.color_type = 'MATERIAL'
         return {'FINISHED'}
