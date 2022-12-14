05ca1714819417fd06d5b1ad8596df5e38a8d7cd
matthias <Weisl@users.noreply.github.com>
2022-12-14 15:00:05 +0200

    Update different blender version exporter version

--- auto_Convex/add_bounding_auto_convex.py
+++ auto_Convex/add_bounding_auto_convex.py
@@ -206,27 +206,35 @@ class VHACD_OT_convex_decomposition(OBJECT_OT_add_bounding_object, Operator):
 
             joined_obj.select_set(True)
 
-            # If blender version is older than 3.3 fallback to old exporter
-            if (3, 3, 0) > bpy.app.version:
-                print("Your Blender version is too old!")
+            # If blender version is older than 3.3 fallback to old exporter addon
+            if (3, 1, 0) > bpy.app.version:
                 import addon_utils
+                
+                # enable the obj addon if it's disabled
                 addon_name = 'io_scene_obj'
                 addon_utils.check(addon_name)
                 success = addon_utils.enable(addon_name)
 
+                # Cancel Operation if addon can't be found
                 if not success:
                     self.report(
                         {'ERROR'}, "The obj export addon is needed for the auto convex to work and was not found")
                     return self.cancel(context)
 
+                # Use export addon
                 bpy.ops.export_scene.obj(filepath=obj_filename, check_existing=False, filter_glob='*.obj;*.mtl', use_selection=True, use_animation=False, use_mesh_modifiers=True, use_edges=True, use_smooth_groups=False, use_smooth_groups_bitflags=False, use_normals=False,
                                          use_uvs=False, use_materials=False, use_triangles=False, use_nurbs=False, use_vertex_groups=False, use_blen_objects=True, group_by_object=False, group_by_material=False, keep_vertex_order=False, global_scale=1.0, path_mode='AUTO', axis_forward='Y', axis_up='Z')
+                
+                # Display a warning when Blender uses the small export instead of the fast one! 
                 self.report(
                     {'WARNING'}, "This version of Blender uses the slow exporter/importer. Update to version 3.3!")
-            else:
+            elif (3, 3, 0) > bpy.app.version:
                 bpy.ops.wm.obj_export(filepath=obj_filename,  check_existing=False, export_selected_objects=True, export_materials=False,
-                                      export_uv=False, export_normals=False, forward_axis='Y', up_axis='Z')
-
+                                      export_uv=False, export_normals=False, forward_axis='Y_FORWARD', up_axis='Z_UP')
+            else: # Blender version is 3.3 or greater
+                bpy.ops.wm.obj_export(filepath=obj_filename,  check_existing=False, export_selected_objects=True, export_materials=False,
+                        export_uv=False, export_normals=False, forward_axis='Y', up_axis='Z')
+                
             if self.prefs.debug:
                 joined_obj.color = (1.0, 0.1, 0.1, 1.0)
                 joined_obj.select_set(False)
@@ -271,12 +279,14 @@ class VHACD_OT_convex_decomposition(OBJECT_OT_add_bounding_object, Operator):
             # List of imported objects
             imported = []
             for obj_path in obj_list:
-                if (3, 3, 0) > bpy.app.version:
+                if (3, 1, 0) > bpy.app.version:
                     bpy.ops.import_scene.obj(filepath=obj_path, use_edges=True, use_smooth_groups=False, use_split_objects=True, use_split_groups=False,
                                              use_groups_as_vgroups=False, use_image_search=False, split_mode='ON', global_clamp_size=0.0, axis_forward='Y', axis_up='Z')
-                else:
+                elif (3, 3, 0) > bpy.app.version:
+                    bpy.ops.wm.obj_import(filepath=obj_path, forward_axis='Y_FORWARD', up_axis='Z_UP')
+                else: # Blender version is 3.3 or greater
                     bpy.ops.wm.obj_import(filepath=obj_path, forward_axis='Y', up_axis='Z')
-
+                
                 imported.append(bpy.context.selected_objects)
 
             # flatten list
