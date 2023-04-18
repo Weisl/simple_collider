7049fe2f1b6b2a98bec35a8f6e1cfa9584d1cb5d
matthias <Weisl@users.noreply.github.com>
2023-04-18 17:35:57 +0300

    Fix missing Alt Shift Ctrl from keymap

--- preferences/keymap.py
+++ preferences/keymap.py
@@ -1,6 +1,6 @@
 import bpy
 
-keys = []
+# keys = []
 
 
 def add_keymap():
@@ -26,13 +26,13 @@ def add_key_to_keymap(idname, kmi, km, active=True):
     ''' Add ta key to the appropriate keymap '''
     kmi.properties.name = idname
     kmi.active = active
-    keys.append((km, kmi))
+    # keys.append((km, kmi))
 
 
 def remove_key(context, idname, properties_name):
     '''Removes addon hotkeys from the keymap'''
     wm = bpy.context.window_manager
-    km = wm.keyconfigs.addon.keymaps['3D View']
+    km = wm.keyconfigs.addon.keymaps['Window']
 
     for kmi in km.keymap_items:
         if kmi.idname == idname and kmi.properties.name == properties_name:
@@ -41,10 +41,12 @@ def remove_key(context, idname, properties_name):
 def remove_keymap():
     '''Removes keys from the keymap. Currently this is only called when unregistering the addon. '''
     # only works for menues and pie menus
-    for km, kmi in keys:
+    wm = bpy.context.window_manager
+    km = wm.keyconfigs.addon.keymaps['Window']
+
+    for kmi in km.keymap_items:
         if hasattr(kmi.properties, 'name') and kmi.properties.name in ['COLLISION_MT_pie_menu', 'VIEW3D_PT_collision_visibility_panel', 'VIEW3D_PT_collision_material_panel']:
             km.keymap_items.remove(kmi)
-    keys.clear()
 
 
 def remove_key(context, idname, properties_name):
