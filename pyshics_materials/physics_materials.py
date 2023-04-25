6ea02eb3ce4b393721fd5b9423d8f3f622735bac
matthias <Weisl@users.noreply.github.com>
2023-04-25 15:51:35 +0300

    Improve physics material window

--- pyshics_materials/physics_materials.py
+++ pyshics_materials/physics_materials.py
@@ -31,10 +31,18 @@ class MATERIAL_OT_physics_material_create(bpy.types.Operator):
 
     my_baseName: bpy.props.StringProperty(name="Name")
 
-
-
     rgb_controller: bpy.props.FloatVectorProperty(name="Color", subtype='COLOR', default=[1, 1, 1, 0.5], size=4, max=1,
                                                   description="Display Color")
+
+    mat_naming_position: bpy.props.EnumProperty(
+        name='Physics Material',
+        items=(('PREFIX', "Prefix", "Prefix"),
+               ('SUFFIX', "Suffix", "Suffix"),
+               ('NONE', "None", "None")),
+        default='PREFIX',
+        description='Add custom naming as prefix or suffix'
+    )
+
     @staticmethod
     def random_color():
         r = random.uniform(0, 1)
@@ -44,19 +52,10 @@ class MATERIAL_OT_physics_material_create(bpy.types.Operator):
         color = (r, g, b, 0.5)
         return color
 
-    def draw(self, context):
-        layout = self.layout
-
-        scene = context.scene
-        prefs = context.preferences.addons[__package__.split('.')[0]].preferences
-        layout.label(text=prefs.physics_material_filter)
-        layout.prop(self, "my_baseName")
-        layout.prop(self, "rgb_controller")
-
-        
-
-
     def invoke(self, context, event):
+        prefs = context.preferences.addons[__package__.split('.')[0]].preferences
+        self.mat_naming_position = prefs.material_naming_position
+        self.final_name = ""
         if context.scene.use_random_color:
             self.rgb_controller = self.random_color()
         else:
@@ -64,8 +63,42 @@ class MATERIAL_OT_physics_material_create(bpy.types.Operator):
 
         return context.window_manager.invoke_props_dialog(self)
 
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+
+        prefs = context.preferences.addons[__package__.split('.')[0]].preferences
+
+        row = layout.row()
+        row.prop(self, "rgb_controller")
+        row = layout.row()
+        row.prop(self, "my_baseName")
+        row = layout.row()
+        row.enabled = False
+        row.prop(prefs, "physics_material_filter", text="Filter")
+        row = layout.row()
+        row.prop(self, 'mat_naming_position', expand=True)
+
+        if self.mat_naming_position == 'PREFIX':
+            row = layout.row()
+            row.label(
+            text="Name = Material Prefix + Basename")
+            name = prefs.physics_material_filter + prefs.physics_material_separator + self.my_baseName
+        elif self.mat_naming_position == 'SUFFIX':  # self.naming_position == 'SUFFIX':
+            row = layout.row()
+            row.label(
+                text="Name = Basename + Material Suffix")
+            name = self.my_baseName + prefs.physics_material_separator + prefs.physics_material_filter
+        else:
+            name = self.my_baseName
+
+        row = layout.row()
+        row.label(text="Name = {}".format(name))
+        self.final_name = name
+
     def execute(self, context):
-        create_material(self.my_baseName, self.rgb_controller)
+        create_material(self.final_name, self.rgb_controller)
         return {"FINISHED"}
 
 
