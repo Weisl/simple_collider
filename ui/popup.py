d97cc47b376c3e050cc49719881c70e94a2f377b
Matthias Patscheider <Weisl@users.noreply.github.com>
2023-05-23 18:42:35 +0300

    #225 Add warning for slow computing

--- ui/popup.py
+++ ui/popup.py
@@ -14,7 +14,7 @@ class VIEW3D_PT_auto_convex_popup(bpy.types.Panel):
 
         colSettings = context.scene.collider_tools
         draw_auto_convex_settings(colSettings, layout)
-
+        layout.label(text='May take up to a few minutes', icon='ERROR')
         layout.operator("collision.vhacd", text="Auto Convex", icon='MESH_ICOSPHERE')
 
         return
