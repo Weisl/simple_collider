5e74d319153c97747a6db7f09929ac48f6c59eb5
Weisl <weisl12@hotmail.de>
2025-03-17 21:55:07 +0200

    fix capsule

--- collider_shapes/add_bounding_capsule.py
+++ collider_shapes/add_bounding_capsule.py
@@ -20,11 +20,10 @@ class OBJECT_OT_add_bounding_capsule(OBJECT_OT_add_bounding_object, Operator):
     bl_description = 'Create bounding capsule colliders based on the selection'
 
     def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.shape = 'capsule_shape'
         self.initial_shape = 'capsule_shape'
 
-
-        super().__init__(*args, **kwargs)
         self.use_space = True
         self.use_modifier_stack = True
         self.use_global_local_switches = True
